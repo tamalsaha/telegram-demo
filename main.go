c5d8ed97c632c5479dfc264e277ae69dd114b071
tamal <tamal@appscode.com>
2018-01-16 04:18:14 -0800

    added-all

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/kimrgrey/go-telegram"
 	"os"
+	"net/url"
 )
 
 var client = telegram.NewClient(os.Getenv("BOT_KEY"))
@@ -11,4 +12,9 @@ var client = telegram.NewClient(os.Getenv("BOT_KEY"))
 func main() {
 	me := client.GetMe()
 	fmt.Println(me.UserName)
+
+	params := url.Values{}
+	params.Set("chat_id", "@mytest")
+	params.Set("text", "Test")
+	client.Call("sendMessage", params, nil)
 }
